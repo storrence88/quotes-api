64367305c88d58c12e455050628f5bc789b86569
Steven Torrence <steve@cratebind.com>
2022-04-23 19:38:44 -0500

    Scope authentication routes to the same scope as the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
+  devise_for :users, path: '/api/v1', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
